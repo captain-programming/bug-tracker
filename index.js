4d68c370e4777de1d4b19588a5869ec54658ed14
Dinesh Karde <dineshkarde103@gmial.com>
2023-01-19 13:25:46 +0530

    backend done

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const dbConnect = require("./config/dbConnect");
-const UserModel = require("./models/user.model");
-const bcrypt = require("bcrypt");
+const UserRoutes = require("./routes/user.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-
-app.get("/", (req, res)=> res.send("Welcome to my server"));
-
-app.post("/signup", async(req, res) =>{
-  const {email, password} = req.body;
-  try{
-    const findUserAccount = await UserModel.findOne({email});
-    if(findUserAccount){
-      return res.status(404).send({message: "User already exist."});
-    }
-
-    const convertHashPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await UserModel.create({email, password: convertHashPassword});
-
-    return res.status(200).send({message: "Signing up successfully"})
-
-  }catch(err){
-    return res.status(500).send(err.message);
-  }
-})
+app.use(express.json());
+app.use("/", UserRoutes);
 
 const PORT = process.env.PORT || 8080;
 
---
+++ routes/user.routes.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const UserModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const UserRoutes = express();
+
+UserRoutes.get("/", (req, res)=> res.send("Welcome to my server"));
+
+//signup
+UserRoutes.post("/signup", async(req, res) =>{
+  const {email, password} = req.body;
+  try{
+    const findUserAccount = await UserModel.findOne({email});
+
+    if(findUserAccount){
+      return res.status(404).send({message: "User already exist."});
+    }
+
+    const convertHashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await UserModel.create({email, password: convertHashPassword});
+
+    return res.status(200).send({message: "Signing up successfully"})
+
+  }catch(err){
+    return res.status(500).send(err.message);
+  }
+})
+
+
+//login
+UserRoutes.post("/login", async(req, res) =>{
+  const {email, password} = req.body;
+  try{
+    const findUserAccount = await UserModel.findOne({email});
+
+    if(!findUserAccount){
+      return res.status(404).send({message: "User does not exist."});
+    }
+
+    const checkPassword= await bcrypt.compare(password, findUserAccount.password);
+
+    if(!checkPassword){
+      return res.status(400).send({message: "Invalid Credentials"})
+    }
+
+    const token = jwt.sign({email: findUserAccount.email, id: findUserAccount._id}, "secrete", {expiresIn: "1 day"});
+
+    return res.status(200).send({message: "Login Successful", token: token})
+
+  }catch(err){
+    return res.status(500).send(err.message);
+  }
+})
+
+module.exports = UserRoutes;
