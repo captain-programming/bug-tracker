e3f1d714fe872e93a3764f5db32ef9a77b8f1836
Dinesh Karde <dineshkarde103@gmial.com>
2023-01-19 17:07:45 +0530

    backend done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,14 +14,14 @@ UserRoutes.post("/signup", async(req, res) =>{
     const findUserAccount = await UserModel.findOne({email});
 
     if(findUserAccount){
-      return res.status(404).send({message: "User already exist."});
+      return res.status(404).send({message: "User already exist.",status: false});
     }
 
     const convertHashPassword = await bcrypt.hash(password, 10);
 
     const newUser = await UserModel.create({email, password: convertHashPassword});
 
-    return res.status(200).send({message: "Signing up successfully"})
+    return res.status(200).send({message: "Signing up successfully", status: true})
 
   }catch(err){
     return res.status(500).send(err.message);
@@ -36,18 +36,18 @@ UserRoutes.post("/login", async(req, res) =>{
     const findUserAccount = await UserModel.findOne({email});
 
     if(!findUserAccount){
-      return res.status(404).send({message: "User does not exist."});
+      return res.status(404).send({message: "User does not exist.", status: false});
     }
 
     const checkPassword= await bcrypt.compare(password, findUserAccount.password);
 
     if(!checkPassword){
-      return res.status(400).send({message: "Invalid Credentials"})
+      return res.status(400).send({message: "Invalid Credentials", status: false})
     }
 
     const token = jwt.sign({email: findUserAccount.email, id: findUserAccount._id}, "secrete", {expiresIn: "1 day"});
 
-    return res.status(200).send({message: "Login Successful", token: token})
+    return res.status(200).send({message: "Login Successful", token: token, status: true})
 
   }catch(err){
     return res.status(500).send(err.message);
